52c38d868937df46b7e46665d7621f95a94de216
Ceren Erdoğan <67686775+erdoganceren@users.noreply.github.com>
2021-01-24 12:04:32 +0300

    Homework Details added.

--- src/main/scala/com/trendyol/bootcamp/homework/ProductMergerJob.scala
+++ src/main/scala/com/trendyol/bootcamp/homework/ProductMergerJob.scala
@@ -5,6 +5,11 @@ object ProductMergerJob {
   def main(args: Array[String]): Unit = {
 
     /**
+    * Bu problem şuan Batch olarak yapılıyor. 
+    * Key,value şeklinde bir datam var. product owner; name'i değiştiriyor, kategorisini değiştirebiliyor. Ben en son halini istiyorum.
+    * Datalarım var elimde 10 satır. Yeni gelenler de var 15 tane. Ortak olmayanlarını target datasına yazıyorum. Ortak olanlarda (yeni gelenler birden fazlaysa timestamp'e göre)
+    * son hallerini alıyorum. id'ye göre gruplanarak yapılır. 
+    * Bu bir merger'dır aslında.
     * Find the latest version of each product in every run, and save it as snapshot.
     *
     * Product data stored under the data/homework folder.
